4a1259b90246d4905f402f7976e5db2433177ced
Vera Voissem <24945513@nwtc.edu>
2024-05-07 20:07:54 -0500

    Trying to add contact form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,42 @@
+$(function() {
+
+
+    $("#form").validate({
+        errorPlacement: function(error, element) {
+            error.appendTo(element.parent());
+            if (element.attr("name") == "gender"
+                || element.attr("name") == "activity[]") {
+                error.prependTo(element.parent());
+            }
+        },
+        success: function(label) {
+            label.parent().removeClass("error-parent");
+        },
+        highlight: function(element, errorClass) {
+            $(element).parent().addClass("error-parent");
+            $(element).parent().find(".error").fadeOut(function() {
+                $(this).fadeIn();
+            });
+        },
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            comments: {
+                required: true
+            }
+        },
+        messages: {
+            email: {
+                required: "Please enter your email",
+            },
+            comments: {
+                required: "Please enter your comment here"
+            },
+    
+        }
+    })});
 
 
 
